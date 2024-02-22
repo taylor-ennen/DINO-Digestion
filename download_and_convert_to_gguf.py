d6490622a147cb5dd324cb7c0526c59c40a89a4b
taylor-ennen <tjennen480@icloud.com>
2024-02-21 20:02:40 -0700

    adding an argument parser, converting existing variables to the argument parser, adjusted order of information logically in response to those changes

--- download_and_convert_to_gguf.py
+++ download_and_convert_to_gguf.py
@@ -1,26 +1,59 @@
-import subprocess
+import argparse
 import logging
 import os
+import subprocess
 from huggingface_hub import snapshot_download
 
 # setup logging
-logging.basicConfig(level=logging.INFO, 
-                    format="%(asctime)s - %(message)s", 
-                    datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
-# alias the folders used for the base models and converted models
-models_folder = "./models_downloaded/"
-converted_models_folder = "./converted_models/"
+# setup parser and add arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "r",
+    "repo_id",
+    default="microsoft/phi-2",
+    type=str,
+    help="Example: microsoft/phi-2"
+)
+parser.add_argument(
+    "o",
+    "outtype",
+    default="f32",
+    choices=["f32", "f16", "q8_0"],
+    type=str,
+    help="Example: f32"
+)
+parser.add_argument(
+    "d",
+    "download_folder",
+    default="./models_downloaded/",
+    type=str,
+    help="Best to just leave this as is \
+unless you have a specific use case"
+)
+parser.add_argument(
+    "c",
+    "converted_models_folder",
+    default="./converted_models/",
+    type=str,
+    help="Best to just leave this as is \
+unless you have a specific use case"
+)
 
-# make sure the folder exists
-if not os.path.exists(models_folder) or not os.path.exists(converted_models_folder):
-    try:
-        os.makedirs(models_folder)
-        os.makedirs(converted_models_folder)
-    except Exception as e:
-        logging.info(f"Error creating root folders needed: {e}")
-else:
-    logging.info(f"Root folders {models_folder} and {converted_models_folder} already exist")
+# parse the arguments and log the input
+args = parser.parse_args()
+model_id = args.r
+outtype = args.o
+models_folder = args.d
+converted_models_folder = args.c
+
+logging.info(model_id)
+logging.info(outtype)
 
 
 # capture the model id from huggingface
@@ -31,6 +64,18 @@ model_name = model_id.split("/")[1]
 logging.info(model_id)
 logging.info(model_name)
 logging.info(model_path)
+
+# make sure the folder exists
+if not os.path.exists(models_folder) or not os.path.exists(converted_models_folder):
+    try:
+        os.makedirs(models_folder)
+        os.makedirs(converted_models_folder)
+    except Exception as e:
+        logging.info(f"Error creating root folders needed: {e}")
+else:
+    logging.info(
+        f"Root folders {models_folder} and {converted_models_folder} already exist"
+    )
 
 # download the model from huggingface, if the model exists on the file system, do not download
 if not os.path.exists(model_path):
@@ -43,7 +88,7 @@ if not os.path.exists(model_path):
             revision="main",
         )
         logging.info(f"Model {model_id} downloaded successfully to {model_path}")
-    # 
+    #i really have mixed feelings about this try except block
     except:
         logging.info(
             f"Could not find model '{model_id}' on huggingface, please check the model id and try again"
@@ -52,12 +97,13 @@ else:
     logging.info(
         f"Model {model_id} already exists on the file system, skipping download \
 if you would like to or need to redownload the model, please delete the folder \
-{model_path} and try again")
+{model_path} and try again"
+    )
 
 # once the model exists, continue with the conversion using the model id as the folder name
 converted_models_output_folder = f"{converted_models_folder}{model_id}/"
-outfile_name=f"{converted_models_output_folder}{model_name}.gguf"
-outtype="f32"
+outfile_name = f"{converted_models_output_folder}{model_name}.gguf"
+outtype = "f32"
 
 bash_convert_to_gguf = f"""python3 llama.cpp/convert-hf-to-gguf.py {model_path}/ \
 --outfile {outfile_name} \
