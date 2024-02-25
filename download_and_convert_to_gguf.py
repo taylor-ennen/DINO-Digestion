2d5043f14c1e0ed280faa61e9cd22bb2e077f911
taylor-ennen <tjennen480@icloud.com>
2024-02-25 10:24:39 -0700

    deleted v1 of the download and convert and renamed -v2 to the base name again---quantization is next.

--- download_and_convert_to_gguf.py
+++ download_and_convert_to_gguf.py
@@ -3,142 +3,208 @@ import logging
 import os
 import subprocess
 from huggingface_hub import snapshot_download
-
+from typing import NoReturn
 # setup logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-)
 
-# setup parser and add arguments
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-r",
-    "-repo_id",
-    default="microsoft/phi-2",
-    type=str,
-    help="Example: microsoft/phi-2",
-)
-parser.add_argument(
-    "-o",
-    "-outtype",
-    default="f32",
-    choices=["f32", "f16", "q8_0"],
-    type=str,
-    help="Example: f32",
-)
-parser.add_argument(
-    "-d",
-    "-download_folder",
-    default="./downloaded_models/",
-    type=str,
-    help="Best to just leave this as is \
-unless you have a specific use case",
-)
-parser.add_argument(
-    "-c",
-    "-converted_models_folder",
-    default="./converted_models/",
-    type=str,
-    help="Best to just leave this as is \
-unless you have a specific use case",
-)
+def setup_logging():
+    """Setup the logging configuration"""
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    return logging
 
-# parse the arguments and log the input
-args = parser.parse_args()
+def setup_parser(parser=argparse.ArgumentParser()) -> argparse.ArgumentParser:
+    """Setup the argument parser and add arguments
+    ----------------
+    Args:
+        parser (argparse.ArgumentParser): the argument parser
+        
+    Returns:
+        argparse.ArgumentParser: the argument parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "-repo_id",
+        default="microsoft/phi-2",
+        type=str,
+        help="Example: microsoft/phi-2",
+    )
+    parser.add_argument(
+        "-o",
+        "-outtype",
+        default="f32",
+        choices=["f32", "f16", "q8_0"],
+        type=str,
+        help="Example: f32",
+    )
+    parser.add_argument(
+        "-d",
+        "-download_folder",
+        default="./downloaded_models/",
+        type=str,
+        help="Best to just leave this as is \
+        unless you have a specific use case",
+    )
+    parser.add_argument(
+        "-c",
+        "-converted_models_folder",
+        default="./converted_models/",
+        type=str,
+        help="Best to just leave this as is \
+        unless you have a specific use case",
+    )
+    return parser
 
-model_id = args.r  # "microsoft/phi-2"
-outtype = args.o  # "f32"
-downloaded_models_folder = args.d  # "./downloaded_models/"
-converted_models_folder = args.c  # "./converted_models/"
+def parse_args(parser: argparse.ArgumentParser) -> argparse.Namespace:
+    """Parse the arguments
+    ----------------
+    Args:
+        parser (argparse.ArgumentParser): the argument parser
+    Returns:
+        argparse.Namespace: the parsed arguments
+    """
+    args = parser.parse_args()
+    return args
 
-model_name = model_id.split("/")[1]  # phi-2
-model_path = f"{downloaded_models_folder}{model_id}/"  # ./downloaded_models/microsoft/phi-2/
-converted_models_output_folder = (
-    f"{converted_models_folder}{model_id}/"  # ./converted_models/microsoft/phi-2/
-)
+def build_convert_to_gguf_command(
+    downloaded_models_output_folder: str, outfile: str, outtype: str
+) -> str:
+    """Build the bash command to convert the model to gguf
+    ----------------
+    Example:
+    python3 llama.cpp/convert-hf-to-gguf.py ./downloaded_models/microsoft/phi-2/ \
+    --outfile ./converted_models/microsoft/phi-2/phi-2_f32.gguf --outtype f32
+    ----------------
+    Args:
+        downloaded_models_output_folder (str): path to the downloaded model
+        outfile (str): path to the output file
+        outtype (str): the type of output file
+        
+    Returns:
+        str: the bash command to convert the model to gguf
+    """
+    # needs to be left justified on new line because of the string formatting
+    return f"""python3 llama.cpp/convert-hf-to-gguf.py {downloaded_models_output_folder} \
+--outfile {outfile} --outtype {outtype}""" 
+
+def output_verification(output_list: list) -> NoReturn:
+    """Take a list of output folders and verify if they exist, if not create them
+    ----------------
+    Args:
+        output_list (list): list of output folders to verify
+        
+    Returns:
+        NoReturn: None
+    """
+    logging.info(
+        f"Checking if the output folders '{output_list}' exist"
+    )
+    for output_directory in output_list:
+        if not os.path.exists(output_directory):
+            try:
+                os.makedirs(output_directory)
+            except Exception as e:
+                logging.info(f"Error creating {output_directory}: {e}")
+        else:
+            logging.info(f"{output_directory} exists, continuing")
+
+def download_full_model_from_huggingface(
+        huggingface_repo: str,
+        local_download_directory: str,
+        use_symlinks: bool = False,
+        revision: str = "main",
+        ) -> NoReturn:
+    """Download the model from huggingface
+    ----------------
+    Args:
+        huggingface_repo (str): the huggingface model id
+        local_download_directory (str): the local download directory
+        use_symlinks (bool): whether to use symlinks or not
+        revision (str): the revision of the model
+    ----------------
+    Returns:
+        NoReturn: None
+    """
+    logging.info(
+        f"Checking if model {huggingface_repo} exists on the file system in {local_download_directory}"
+    )
+    if not os.path.exists(local_download_directory):
+        logging.info(
+            f"Model {huggingface_repo} does not exist on the file system, downloading..."
+        )
+        # using a try except block to capture if the model exists on huggingface
+        try:
+            snapshot_download(
+                repo_id=huggingface_repo,
+                local_dir=local_download_directory,
+                local_dir_use_symlinks=False,
+                revision="main",
+            )
+            logging.info(
+                f"Model {huggingface_repo} downloaded successfully to {local_download_directory}"
+            )
+        except Exception as e:
+            logging.info(
+                f"Could not find model '{huggingface_repo}' on huggingface, please check the model id and try again"
+            )
+            logging.info(f"Error: {e}")
+            exit()
+    else:
+        logging.info(
+            f"Model {huggingface_repo} already exists on the file system, skipping download \
+    if you would like to or need to redownload the model, please delete the folder \
+    {local_download_directory} and try again"
+        )
+    logging.info(
+        f"Model {huggingface_repo} exists on the file system in {local_download_directory}"
+    )
+
+def convert_to_gguf(bash_command: str) -> NoReturn:
+    """Convert the model to gguf
+    ----------------
+    Args:
+        bash_command (str): the bash command to convert the model to gguf
+    ----------------
+    Returns:
+        NoReturn: None
+    """
+    logging.info(
+        f"Starting Conversion...this may take a while..do not close the terminal window until you see confirmation, thanks!"
+    )
+    output = subprocess.check_output(["bash", "-c", bash_command])
+    logging.info(output)
+    logging.info(f"Conversion completed...converted model saved to {outfile}")
+
+logging = setup_logging()
+parser = setup_parser()
+args = parse_args(parser)
+
+huggingface_repo_id, outtype, downloaded_models_folder, converted_models_folder = args.r, args.o, args.d, args.c
+
+model_name = huggingface_repo_id.split("/")[1]  # phi-2
+downloaded_models_output_folder = f"{downloaded_models_folder}{huggingface_repo_id}/"  # ./downloaded_models/microsoft/phi-2/
+converted_models_output_folder = f"{converted_models_folder}{huggingface_repo_id}/"  # ./converted_models/microsoft/phi-2/
 outfile = f"{converted_models_output_folder}{model_name}_{outtype}.gguf"  # ./converted_models/microsoft/phi-2/phi-2_f32.gguf
-
-# -create the conversion command using our arguments
-bash_convert_to_gguf = f"""python3 llama.cpp/convert-hf-to-gguf.py {model_path} \
---outfile {outfile} \
---outtype {outtype}"""
-# example output of the bash command:
-# python3 llama.cpp/convert-hf-to-gguf.py ./downloaded_models/microsoft/phi-2/ \
-# --outfile ./converted_models/microsoft/phi-2/phi-2_f32.gguf \
-# --outtype f32
+conversion_bash_command=build_convert_to_gguf_command(downloaded_models_output_folder, outfile, outtype)
 
 # stupidly verbose logging for all the inputs, i dont like this formatting also.
 logging.info(
-    "Arguments: {}, Model ID: {}, Outtype: {}, Download Folder: {}, Converted Models Folder: {}, Model Path: {}, Model Name: {}, Converted Models Output Folder: {}, Outfile Name: {}, Bash Command: {}".format(
+    "Arguments: {}, Model ID: {}, Outtype: {}, Download Folder: {}, Converted Models Folder: {}, Model Path: {}, Model Name: {}, Converted Models Output Folder: {}, Outfile Name: {}, Conversion Command Build: {}".format(
         args,
-        model_id,
+        huggingface_repo_id,
         outtype,
         downloaded_models_folder,
         converted_models_folder,
-        model_path,
+        downloaded_models_output_folder,
         model_name,
         converted_models_output_folder,
         outfile,
-        bash_convert_to_gguf,
+        conversion_bash_command,
+        
     )
 )
 
-# verify the folders exist, if not create them
-logging.info(
-    f"Checking if the root folders {downloaded_models_folder} and {converted_models_folder} exist"
-)
-if not os.path.exists(downloaded_models_folder) or not os.path.exists(converted_models_folder):
-    try:
-        os.makedirs(downloaded_models_folder)
-        os.makedirs(converted_models_folder)
-    except Exception as e:
-        logging.info(f"Error creating root folders needed: {e}")
-else:
-    logging.info(
-        f"Root folders {downloaded_models_folder} and {converted_models_folder} already exist"
-    )
-
-# download the model from huggingface, if the model exists on the file system, do not download
-logging.info(f"Checking if model {model_id} exists on the file system in {model_path}")
-if not os.path.exists(model_path):
-    logging.info(f"Model {model_id} does not exist on the file system, downloading...")
-    # using a try except block to capture if the model exists on huggingface
-    try:
-        snapshot_download(
-            repo_id=model_id,
-            local_dir=model_path,
-            local_dir_use_symlinks=False,
-            revision="main",
-        )
-        logging.info(f"Model {model_id} downloaded successfully to {model_path}")
-    except Exception as e:
-        logging.info(
-            f"Could not find model '{model_id}' on huggingface, please check the model id and try again"
-        )
-        logging.info(f"Error: {e}")
-        exit()
-else:
-    logging.info(
-        f"Model {model_id} already exists on the file system, skipping download \
-if you would like to or need to redownload the model, please delete the folder \
-{model_path} and try again"
-    )
-logging.info(f"Model {model_id} exists on the file system in {model_path}")
-
-if not os.path.exists(converted_models_output_folder):
-    try:
-        os.makedirs(converted_models_output_folder)
-    except Exception as e:
-        logging.info(f"Error creating converted models folder: {e}")
-else:
-    logging.info(
-        f"Converted models folder {converted_models_output_folder} exists"
-    )
-logging.info(
-    "Starting Conversion...this may take a while..do not close the terminal window until you see confirmation, thanks!"
-)
-
-output = subprocess.check_output(["bash", "-c", bash_convert_to_gguf])
-
-logging.info(output)
-logging.info(f"Conversion completed...converted model saved to {outfile}")
+output_verification([downloaded_models_folder, converted_models_folder, converted_models_output_folder])
+download_full_model_from_huggingface(huggingface_repo_id, downloaded_models_output_folder)
+convert_to_gguf(conversion_bash_command)
